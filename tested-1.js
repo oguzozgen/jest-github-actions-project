28114333470bf985b93867baf2f0e3f7a4bde08e
Oguz Ozgen <oguz.ozgen@loqusgroup.com>
2022-11-10 01:08:57 +0300

    on first commint, only tested-1.js appear

--- tested-1.js
+++ tested-1.js
@@ -7,6 +7,10 @@ const multiplyPositive = (first, second) => {
         return "Values must be positive";
     }
 
+    if (!first >= 1 || !second >= 1) {
+        return "Values must be positive";
+    }
+
     if (typeof (first) !== "number" || typeof (second) !== "number") {
         return "Values types must be number";
     }
