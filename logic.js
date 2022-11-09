47f570f7492536596e68403aab42ec13b584806f
Oguz Ozgen <oguz.ozgen@loqusgroup.com>
2022-11-09 23:01:50 +0300

    pr comment test

--- logic.js
+++ logic.js
@@ -15,6 +15,22 @@ const sumPositive = (first, second) => {
     return first + second;
 };
 
+const multiplyPositive = (first, second) => {
+    if (!first || !second) {
+        return "Both values must be exist";
+    }
+
+    if (!first >= 1 || !second >= 1) {
+        return "Values must be positive";
+    }
+
+    if (typeof (first) !== "number" || typeof (second) !== "number") {
+        return "Values types must be number";
+    }
+
+    return first * second;
+};
+
 module.exports = {
     sumPositive
 };
