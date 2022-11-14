193c3b43e53446dfd6652dc62b0fb5486b5d01e2
Oguz Ozgen <oguz.ozgen@loqusgroup.com>
2022-11-14 18:33:39 +0300

    config changed

--- jest.config.js
+++ jest.config.js
@@ -26,11 +26,11 @@ module.exports = {
     collectCoverage: true,
 
     // An array of glob patterns indicating a set of files for which coverage information should be collected
-    collectCoverageFrom: [
-        '**/*.{js}',
-        '!**/node_modules/**',
-        '!**/vendor/**',
-    ],
+    //collectCoverageFrom: [
+    //    '**/*.{js}',
+    //    '!**/node_modules/**',
+    //    '!**/vendor/**',
+    //],
 
     // The directory where Jest should output its coverage files
     coverageDirectory: "coverage",
