f2006ad83000a882dd7088e7e8432668be966d99
Oguz Ozgen <oguz.ozgen@loqusgroup.com>
2022-11-14 11:14:07 +0300

    github actions creates the coverage files for logic.js

--- logic.js
+++ logic.js
@@ -12,6 +12,10 @@ const sumPositive = (first, second) => {
         return "Values types must be number";
     }
 
+    if (typeof (first) !== "number" || typeof (second) !== "number") {
+        return "Values types must be numbers";
+    }
+
     return first + second;
 };
 
