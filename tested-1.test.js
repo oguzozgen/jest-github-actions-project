b923b12d884f2de0a24b1badf00b91f0e173dd6f
Oguz Ozgen <oguz.ozgen@loqusgroup.com>
2022-11-15 12:43:34 +0300

    coverage increased

--- tested-1.test.js
+++ tested-1.test.js
@@ -2,7 +2,7 @@ let tested1 = require("./tested-1");
 
 describe("logic module tests", function () {
 
-   /* test("if first value not exist, Response will be string error message", function () {
+    test("if first value not exist, Response will be string error message", function () {
         expect(tested1.multiplyPositive(3, 2)).toBe(6);
     });
 
@@ -12,5 +12,5 @@ describe("logic module tests", function () {
 
     test("When first value type string, Response will be string error message", function () {
         expect(tested1.multiplyPositive("asd", "asd")).toBe("Values types must be number");
-    });*/
+    });
 });
